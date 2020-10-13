683f33714e3bcffeaa169d53dcc623619746632f
nazpari <72775898+nazpari@users.noreply.github.com>
2020-10-13 02:30:58 +0000

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld
 {
 	public static void main(String[] args) {
-		System.out.println("This is my first project");
+		System.out.println("This is my latest first project");
 	}
 }
